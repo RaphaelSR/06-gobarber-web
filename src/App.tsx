55164225015d39bd5ecf0672a621f4e7f7ae09b3
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-10-20 13:09:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SigIn from './pages/Sigin';
+import GlobalStyle from './styles/global';
+
+
+const App: React.FC = () => (
+  <>
+  <SigIn/>
+  <GlobalStyle />
+  </>
+)
+
+export default App;
