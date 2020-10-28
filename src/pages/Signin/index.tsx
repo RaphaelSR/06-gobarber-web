4ad9ac932ca5f5b565daa7cec942e9c00196d5cd
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-10-27 22:31:12 -0300

    API pelo contexto

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,61 +1,88 @@
-import React, {useCallback, useRef} from 'react';
-import Input from '../../components/Input';
-import Button from '../../components/Button';
-import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
-import logoImg from '../../assets/logo.svg';
-import {Form} from '@unform/web'
+import React, { useCallback, useRef, useContext } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Container, Content, Background } from './styles';
+
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../util/getValidationErrors';
 
+import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
+import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
-    const formRef = useRef<FormHandles>(null);
+  const formRef = useRef<FormHandles>(null);
 
-    const handleSubmit = useCallback( async (data: object) => {
-         try{
-             formRef.current?.setErrors({});
-             const schema = Yup.object().shape({
-                 name: Yup.string().required('Nome obrigatório'),
-                 email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
-                 password: Yup.string().required('Senha obrigatória'),
-             });
-                        
-             await schema.validate(data, {abortEarly: false});
- 
- 
-         }catch (err){
-             const errors = getValidationErrors(err);
-             formRef.current?.setErrors(errors);
-             console.log(err);
-         }
-     }, []);
+  const { signIn } = useContext(AuthContext);
 
-return (
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await signIn({
+        email: data.email,
+        password: data.password,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    }
+  }, []);
+
+  return (
     <Container>
-        <Content>
-            <img src={logoImg} alt="GoBarber"/>    
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-            <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu logon</h1>  
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-            <Input name="email" icon={FiMail} placeholder="E-mail"/>
-            <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>  
-            <Button type="submit" >Entrar</Button>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-            <a href="forgot">Esqueci minha senha</a>
-            </Form>        
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-            <a href="login">
-                <FiLogIn/>
-                Criar conta</a>
-        </Content>    
-        <Background />   
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="signup">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
     </Container>
-)};
+  );
+};
 
 export default SignIn;
