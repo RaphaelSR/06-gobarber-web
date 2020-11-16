5d4c324f417b845de95d8ef7f72f0344e329ead7
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-11-16 16:48:52 -0300

    Estrutura de Toasts
    8:44

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignIn from './pages/Signin';
 //import SignUp from './pages/Signup';
+import ToastContainer from './components/ToastContainer';
 import GlobalStyle from './styles/global';
 import { AuthProvider } from './hooks/AuthContext';
 
@@ -12,6 +13,8 @@ const App: React.FC = () => (
   <AuthProvider>
   <SignIn/>
   </AuthProvider>
+
+  <ToastContainer/>
 
   <GlobalStyle />
   </>
